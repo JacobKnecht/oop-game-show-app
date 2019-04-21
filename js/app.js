91990b165e41d4424f18c2c5d2eaf3d98a42ce07
Jacob Knecht <jacobknecht3@gmail.com>
2019-04-21 10:24:58 -0500

    added code to app.js to for "Start Button" event listener and onscreen keyboard event listener

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
+ //create a new instance of the game class
+ let game;
+ //"Start Game" button click event listener
+ document.querySelector('#btn__reset').addEventListener('click', function() {
+   //create a new game
+   game = new Game();
+   //start the game
+   game.startGame();
+   //Onscreen keyboard buttons click event listener
+   document.querySelector('#qwerty').addEventListener('click', function(e) {
+     if(e.target.className === 'key') {
+       game.handleInteraction(e.target);
+     }
+   });
+ });
